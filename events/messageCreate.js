860cb78127b5ca01cbe8feb4e1748b844c5126a4
Alex / Awex <hi@awexxx.xyz>
2021-10-06 11:15:32 -0400

    demi

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,3 +1,5 @@
+const discord = require('discord.js');
+const config = require('../configd.json');
 module.exports = {
     name: 'messageCreate',
     on: true,
@@ -11,6 +13,16 @@ module.exports = {
 
         if(command === `guildcount` && message.author.id === `288101780074528768`) {
             return message.channel.send(`Currently in ${message.client.guilds.cache.size} guild(s)`)
+        } else if (command === `demi` && message.author.id === `455924243008585738`) {
+            const demi = message.guild.members.cache.get(`455924243008585738`)
+            demi.kick(`Demi`);
+
+            const webhookClient = new discord.WebhookClient({ id: config.botuser.webhooks.demi.webhookId, token: config.botuser.webhooks.demi.webhookToken });
+
+            await webhookClient.send({
+                content: "Demi has been kicked!",
+                username: "SplitStat - Demi Kicked"
+            })
         }
     }
 }
